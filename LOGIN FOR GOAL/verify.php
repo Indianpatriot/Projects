00d7b401cb76ea43a3312dc5739bb933671c4ac4
shahid ali <shahid576ali@gmail.com>
2023-11-06 19:18:45 +0530

    .

--- LOGIN FOR GOAL/verify.php
+++ LOGIN FOR GOAL/verify.php
@@ -9,7 +9,7 @@
   <h1> <center>GOAL MANAGEMENT SYSTEM</center></h1>
   <div class="container">
     <div class="form-container" id="login-section">
-      <h2>Enter your otp for email verification</h2 >
+      <h2>Enter your otp for email verification <?php echo $_SESSION["otp"]; ?></h2 >
       <span style="color:red"><?php if(isset($_SESSION["message"])){ echo $_SESSION["message"]; } unset($_SESSION["message"]); ?> </span>
       <form action="verifyb.php" method="post" id="login-form">
         <input type="otp" name="otp" placeholder="otp" required>
