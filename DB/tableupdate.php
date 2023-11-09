50a986f6ffab6d8be21cfa10217189bb74fcedb6
shahid ali <shahid576ali@gmail.com>
2023-11-09 21:23:41 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -2,7 +2,12 @@
 
 include("dbconn.php");
 
-    $sql = "DROP TABLE `goal_parameter`";
+    $sql = "CREATE TABLE `goal_parameter` (
+        `parameter_id` int(10) NOT NULL,
+        `team_id` int(10) NOT NULL,
+        `parameter` varchar(50) NOT NULL,
+        `parameter_data_type` varchar(50) NOT NULL
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci";
     if($conn->query($sql)){
         echo "done";
     }else{
