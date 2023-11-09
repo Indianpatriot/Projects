5fab7f8d492885c6ea57ae239d6a5843f32599fc
shahid ali <shahid576ali@gmail.com>
2023-11-09 18:45:48 +0530

    .

--- DB/tabledata.php
+++ DB/tabledata.php
@@ -2,7 +2,7 @@
 
 include("dbconn.php");
 
-$query = "SELECT * FROM `gd1`";
+$query = "SELECT * FROM `city of joy`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
