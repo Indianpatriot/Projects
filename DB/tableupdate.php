a60cc119d3787f0968a558dfc1ac54e9057a93d8
shahid ali <shahid576ali@gmail.com>
2023-12-01 16:01:44 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -2,17 +2,7 @@
 
 include("dbconn.php");
 
-    $sql = "CREATE TABLE `city of joy` (
-        `ID` int(10) NOT NULL,
-        `Member ID` int(10) NOT NULL,
-        `Member Name` varchar(50) NOT NULL,
-        `Date` date NOT NULL,
-        `leads` int(50) DEFAULT NULL,
-        `Cities ongoing` varchar(50) DEFAULT NULL,
-        `cities started` varchar(50) DEFAULT NULL,
-        `Centres` varchar(50) DEFAULT NULL,
-        `Sessions` varchar(50) DEFAULT NULL
-      )";
+    $sql = "REPAIR TABLE `central team simtrak`";
 
     if($conn->query($sql)){
         echo "done";
