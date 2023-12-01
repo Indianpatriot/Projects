d55fe04c9d6fdf97eaeb948ac76f20f8c6c54592
shahid ali <shahid576ali@gmail.com>
2023-12-01 15:43:57 +0530

    .

--- DB/tableatr.php
+++ DB/tableatr.php
@@ -7,7 +7,7 @@ $sql = "SHOW TABLES";
 $result = $conn->query($sql);
 
 // Check if there are tables
-$columnsSql = "SHOW COLUMNS FROM `central_team_simtrak`";
+$columnsSql = "SHOW COLUMNS FROM `city of joy`";
         $columnsResult = $conn->query($columnsSql);
 
         if ($columnsResult->num_rows > 0) {
