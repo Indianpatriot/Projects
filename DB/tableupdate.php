e956a599e6876fb3994cee014d6fa4acc8ddf42b
shahid ali <shahid576ali@gmail.com>
2023-12-01 16:04:31 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -2,7 +2,22 @@
 
 include("dbconn.php");
 
-    $sql = "REPAIR TABLE `central team simtrak`";
+    $sql = "CREATE TABLE `echo` (
+        `ID` int(10) NOT NULL,
+        `Member ID` int(10) NOT NULL,
+        `Member Name` varchar(50) NOT NULL,
+        `Date` date NOT NULL DEFAULT current_timestamp(),
+        `LSeT Website` varchar(50) NOT NULL,
+        `ADORE Website` varchar(50) NOT NULL,
+        `Simtrak Website` varchar(50) NOT NULL,
+        `ASCEND website` varchar(50) NOT NULL,
+        `Weeho Portal` varchar(50) NOT NULL,
+        `Foreword website` varchar(50) NOT NULL,
+        `Goal Management System` varchar(50) NOT NULL,
+        `Weeho App` varchar(50) NOT NULL,
+        `Simtrak app` varchar(50) NOT NULL,
+        primary key(ID)
+      )";
 
     if($conn->query($sql)){
         echo "done";
