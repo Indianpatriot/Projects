1dcc44003c340d2cccbf89ea5f35aac17666c125
shahid ali <shahid576ali@gmail.com>
2023-12-01 20:16:03 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -2,10 +2,18 @@
 
 include("dbconn.php");
 
-    $sql = "CREATE TABLE `role_teams` (
-        `user_id` int(10) NOT NULL,
-        `role_id` int(11) DEFAULT NULL,
-        `team_id` int(11) DEFAULT NULL
+    $sql = "CREATE TABLE `simtrak recuitment` (
+        `ID` int(10) NOT NULL,
+        `Member ID` int(10) NOT NULL,
+        `Member Name` varchar(50) NOT NULL,
+        `Date` date NOT NULL,
+        `Leads` varchar(50) NOT NULL,
+        `Domain` varchar(50) NOT NULL,
+        `Int Sch` varchar(50) NOT NULL,
+        `Int con.` varchar(50) NOT NULL,
+        `Selected` varchar(50) NOT NULL,
+        `Orientation` varchar(50) NOT NULL,
+        `Joined` varchar(50) NOT NULL
       )";
 
     if($conn->query($sql)){
