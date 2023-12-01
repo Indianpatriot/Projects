94fc1e0ddbfe19463d6c75c229d2eb98743771b7
shahid ali <shahid576ali@gmail.com>
2023-12-01 15:04:41 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -2,7 +2,21 @@
 
 include("dbconn.php");
 
-    $sql = "drop table `central team simtrak`";
+    $sql = "CREATE TABLE `central team simtrak` (
+        `ID` int(10) NOT NULL,
+        `Member ID` int(10) NOT NULL,
+        `Member Name` varchar(50) NOT NULL,
+        `Date` date NOT NULL DEFAULT current_timestamp(),
+        `New tasks received` int(10) NOT NULL,
+        `Tasks in progress` int(10) NOT NULL,
+        `Continuous tasks` int(10) NOT NULL,
+        `Tasks completed today` int(10) NOT NULL,
+        `Article recieved` int(50) DEFAULT NULL,
+        `Cities initiated` varchar(50) DEFAULT NULL,
+        `Cities started` varchar(50) DEFAULT NULL,
+        primary key ('ID')
+      ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+      ";
 
     if($conn->query($sql)){
         echo "done";
