a5193d3c0dd2d80be06b5910cc4ba00826bc690c
shahid ali <shahid576ali@gmail.com>
2023-12-01 21:33:33 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -2,8 +2,8 @@
 
 include("dbconn.php");
 
-    $sql = "ALTER TABLE `simtrak recuitment`
-    MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT";
+    $sql = "ALTER TABLE `users`
+    MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37";
 
     if($conn->query($sql)){
         echo "done";
