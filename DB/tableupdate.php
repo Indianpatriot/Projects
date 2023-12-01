31477d33ecfb435ed016d5048936afefdbf2d102
shahid ali <shahid576ali@gmail.com>
2023-12-01 20:10:58 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -2,18 +2,9 @@
 
 include("dbconn.php");
 
-    $sql = "CREATE TABLE `lset plan` (
-        `ID` int(10) NOT NULL,
-        `Member ID` int(10) NOT NULL,
-        `Member Name` varchar(50) NOT NULL,
-        `Date` int(10) NOT NULL,
-        `Leads` int(10) NOT NULL,
-        `Non lset form filled` int(10) NOT NULL,
-        `Forms filled` int(10) NOT NULL,
-        `Screening` int(10) NOT NULL,
-        `Final Int` int(10) NOT NULL,
-        `Selected` int(10) NOT NULL,
-        `Joining` int(10) NOT NULL
+    $sql = "CREATE TABLE `roles` (
+        `id` int(11) NOT NULL,
+        `role_name` varchar(50) NOT NULL
       )";
 
     if($conn->query($sql)){
