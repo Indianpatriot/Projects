24020bf5dfba87fc4a939c4c947aa53927770592
shahid ali <shahid576ali@gmail.com>
2023-12-25 21:36:48 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,12 +3,27 @@
 
     include("dbconn.php");
 
-    $sql = "INSERT INTO `roles` (`id`, `role_name`) VALUES
-        (1, 'Admin'),
-        (2, 'Subadmin'),
-        (3, 'Manager'),
-        (4, 'Member'),
-        (5, 'noramal user')";
+    $sql = "INSERT INTO `role_teams` (`user_id`, `role_id`, `team_id`) VALUES
+    (10, 1, -1),
+    (11, 2, 1),
+    (11, 2, 2),
+    (16, 2, 3),
+    (18, 5, 0),
+    (19, 3, 2),
+    (20, 3, 3),
+    (21, 4, 1),
+    (22, 4, 1),
+    (23, 4, 2),
+    (24, 4, 2),
+    (25, 4, 3),
+    (29, 5, 0),
+    (30, 4, 1),
+    (31, 4, 6),
+    (32, 4, 6),
+    (33, 4, 6),
+    (34, 5, 0),
+    (35, 3, 6),
+    (36, 5, 0)";
 
     if($conn->query($sql)){
         echo "done ok";
