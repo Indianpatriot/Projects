af5d2b5070cea7f7f9a2111adca480eb1b154346
shahid ali <shahid576ali@gmail.com>
2023-12-25 23:46:58 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,9 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "INSERT INTO `central team simtrak` (`ID`, `Member ID`, `Member Name`, `Date`, `New tasks received`, `Tasks in progress`, `Continuous tasks`, `Tasks completed today`, `Article recieved`, `Cities initiated`, `Cities started`) VALUES
-    (13, 0, '', '2023-09-25', 1, 2, 3, 4, NULL, NULL, NULL),
-    (19, 21, 'mahati', '2023-10-11', 5, 4, 0, 3, 5, NULL, NULL)";
+    $sql = "UPDATE `teams` SET `team_name` = 'Central team simtrak' WHERE `teams`.`id` = 1";
 
     if($conn->query($sql)){
         echo "done ok";
