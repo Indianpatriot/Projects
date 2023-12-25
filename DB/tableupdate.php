97644a97a8eea72b6bb408d93d76716d6f3d7ae4
shahid ali <shahid576ali@gmail.com>
2023-12-25 22:18:06 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,9 @@
 
     include("dbconn.php");
 
-    $sql = "DELETE FROM goal_parameter WHERE `goal_parameter`.`parameter_id` = 10";
+    $sql = "INSERT INTO `central team simtrak` (`ID`, `Member ID`, `Member Name`, `Date`, `New tasks received`, `Tasks in progress`, `Continuous tasks`, `Tasks completed today`, `Article recieved`, `Cities initiated`, `Cities started`) VALUES
+    (13, 0, '', '2023-09-25', 1, 2, 3, 4, NULL, NULL, NULL),
+    (19, 21, 'mahati', '2023-10-11', 5, 4, 0, 3, 5, NULL, NULL)";
 
     if($conn->query($sql)){
         echo "done ok";
