e967f0255dadac08ef828dd10a311a27b53dc6ae
shahid ali <shahid576ali@gmail.com>
2023-12-25 21:51:44 +0530

    .

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,27 +3,9 @@
 
     include("dbconn.php");
 
-    $sql = "INSERT INTO `role_teams` (`user_id`, `role_id`, `team_id`) VALUES
-    (10, 1, -1),
-    (11, 2, 1),
-    (11, 2, 2),
-    (16, 2, 3),
-    (18, 5, 0),
-    (19, 3, 2),
-    (20, 3, 3),
-    (21, 4, 1),
-    (22, 4, 1),
-    (23, 4, 2),
-    (24, 4, 2),
-    (25, 4, 3),
-    (29, 5, 0),
-    (30, 4, 1),
-    (31, 4, 6),
-    (32, 4, 6),
-    (33, 4, 6),
-    (34, 5, 0),
-    (35, 3, 6),
-    (36, 5, 0)";
+    $sql = "INSERT INTO `city of joy` (`ID`, `Member ID`, `Member Name`, `Date`, `leads`, `Cities ongoing`, `cities started`, `Centres`, `Sessions`) VALUES
+    (1, 31, 'Tanishaa Gupta', '0000-00-00', 454, '455', '22', '44', '55'),
+    (2, 35, 'Bono Jyoshna Biswal', '0000-00-00', 33, '66', 'HHKS', '77', '22')";
 
     if($conn->query($sql)){
         echo "done ok";
