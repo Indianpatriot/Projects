fb1bc373a2800f8cd693c89e2bdda947253da2f3
shahid ali <shahid576ali@gmail.com>
2023-12-25 21:57:40 +0530

    .

--- DB/tabledata.php
+++ DB/tabledata.php
@@ -2,7 +2,7 @@
 
 include("dbconn.php");
 
-$query = "SELECT * FROM `echo`";
+$query = "SELECT * FROM `gd1`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
