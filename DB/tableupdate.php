f160b1b20bca3b39945302943165a2070efed952
shahid ali <shahid576ali@gmail.com>
2023-12-26 13:42:19 +0530

    ....

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "UPDATE `teams` SET `team_name` = 'Central team simtrak' WHERE `teams`.`id` = 1";
+    $sql = "DELETE FROM teams WHERE `teams`.`id` = 1";
 
     if($conn->query($sql)){
         echo "done ok";
