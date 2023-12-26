904033cc55dc5db20d59637e06ffe4eded0bf2f4
shahid ali <shahid576ali@gmail.com>
2023-12-26 16:05:10 +0530

    ...

--- DB/tableatr.php
+++ DB/tableatr.php
@@ -7,7 +7,7 @@ $sql = "SHOW TABLES";
 $result = $conn->query($sql);
 
 // Check if there are tables
-$columnsSql = "SHOW COLUMNS FROM `Simtrak Recuitment`";
+$columnsSql = "SHOW COLUMNS FROM `Central team simtrak`";
         $columnsResult = $conn->query($columnsSql);
 
         if ($columnsResult->num_rows > 0) {
--- DB/tabledata.php
+++ DB/tabledata.php
@@ -2,7 +2,7 @@
 
 include("dbconn.php");
 
-$query = "SELECT * FROM `Simtrak Recuitment`";
+$query = "SELECT * FROM `Central team simtrak`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
