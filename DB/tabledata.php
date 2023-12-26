9b1796b222d3b6db4bff3526a0f5a32ec143c6e1
shahid ali <shahid576ali@gmail.com>
2023-12-26 14:03:38 +0530

    ...

--- DB/tabledata.php
+++ DB/tabledata.php
@@ -2,17 +2,11 @@
 
 include("dbconn.php");
 
-$teamID = $_SESSION["team_id"];
-$sql1 = "SELECT * FROM `teams` WHERE `id` = '$teamID'";
-echo $teamID;
-$teamname = mysqli_query($conn,$sql1);
-$teamname = mysqli_fetch_object($teamname);
-echo $teamname->team_name;
-$query = "SELECT * FROM `Central team simtrak`";
+$query = "SELECT * FROM `central_team_simtrak`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
-    $rows = mysqli_fetch_object($result);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
 
     // Print the contents using print_r
     echo "<pre>";
