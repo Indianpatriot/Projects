e459e1ca698d69852fac77d516620886d426dd43
shahid ali <shahid576ali@gmail.com>
2023-12-26 13:44:34 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,8 @@
 
     include("dbconn.php");
 
-    $sql = "DELETE FROM teams WHERE `teams`.`id` = 1";
+    $sql = "INSERT INTO `teams` (`id`, `team_name`, `team_domain`, `Status`) VALUES
+    (1, 'Central team simtrak', 'domain1', 'Active')";
 
     if($conn->query($sql)){
         echo "done ok";
