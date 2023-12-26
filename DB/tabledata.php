96e743c0cb98fe5ff2f8019be2cc87a691d81d33
shahid ali <shahid576ali@gmail.com>
2023-12-26 14:06:47 +0530

    ...

--- DB/tabledata.php
+++ DB/tabledata.php
@@ -2,7 +2,7 @@
 
 include("dbconn.php");
 
-$query = "SELECT * FROM `Central team simtrak`";
+$query = "SELECT * FROM `central team simtrak`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
