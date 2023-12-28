d0f2a4a179b2d28b4a9cc8fa92fbb34bed4b0ca4
shahid ali <shahid576ali@gmail.com>
2023-12-28 21:03:12 +0530

    ...

--- indexb.php
+++ indexb.php
@@ -23,16 +23,10 @@
             $teamID = $_REQUEST["team_name"];
         }
             do{ 
-                echo $teamID;
                 $sql1 = "SELECT * FROM `teams` WHERE `id` = '$teamID'";
                 $teamname = mysqli_query($conn,$sql1);
                 $teamname = mysqli_fetch_object($teamname);
                 if(isset($_REQUEST[$parameter])){
-                    // echo $j; ?> <br> <?php
-                    // echo $_REQUEST[$parameter];
-                    // echo $_REQUEST[$data_type];
-                    // echo $_REQUEST["team_name"];
-                    // echo $_REQUEST["team_mentor"];
                     if($_REQUEST[$data_type]=="DATE"){
                         $sql1 ="ALTER TABLE `$teamname->team_name` ADD `$_REQUEST[$parameter]` $_REQUEST[$data_type]  DEFAULT CURRENT_TIMESTAMP";
                     }else{                    
@@ -40,10 +34,10 @@
                     }
                         $sql2 ="INSERT INTO `goal_parameter`(`team_id`, `parameter`, `parameter_data_type`) VALUES ('$teamID','".$_REQUEST[$parameter]."','".$_REQUEST[$data_type]."')";
                     if(mysqli_query($conn ,$sql1)){
-                        echo "sql1";
+                    
                     }
                     if(mysqli_query($conn ,$sql2)){
-                        echo "sql2";
+                        
                     }
                 }    
                 $j++; 
