f5b439dacc4d76e001c755daefe0d353cf45c843
shahid ali <shahid576ali@gmail.com>
2023-12-28 21:30:23 +0530

    1232

--- login.php
+++ login.php
@@ -1,22 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login and Sign Up</title>
   <link rel="stylesheet" type="text/css" href="styles.css">
+  <meta http-equiv="Cache-control" content="no-cache">
   <?php session_start(); ?>
+  <style>
+    body {
+     
+    }
+  </style>
 </head>
 <body>
-  <h1> <center>GOAL MANAGEMENT SYSTEM</center></h1>
+  <h1 > <center>GOAL MANAGEMENT SYSTEM</center></h1>
   <div class="container">
     <div class="form-container" id="login-section">
-      <h2>Login</h2 >
-      <span style="color:red"><?php if(isset($_SESSION["message"])){ echo $_SESSION["message"]; } unset($_SESSION["message"]); ?> </span>
+      <h2>Login</h2>
+      <span style="color:red"><?php if(isset($_SESSION["message"])){ echo $_SESSION["message"]; } unset($_SESSION["message"]); ?></span>
       <form action="loginb.php" method="post" id="login-form">
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <p><a href="sendotp.php">forgot password?</a></p>
+        <input type="text" name="username" placeholder="Username" required value="<?php echo isset($_SESSION['username']) ? $_SESSION['username'] : ''; ?>">
+        <input type="password" name="password" id="login-password" placeholder="Password" required>
+        <input type="checkbox" style="display:inline;" onclick="showPassword('login-password') "> Show password
         <button type="submit">Login</button>
         <p>Don't have an account? <a href="#" id="signup-link">Sign Up</a></p>
+        <p><a href="sendotp.php">Forgot password?</a></p>
       </form>
     </div>
     <div class="form-container hidden" id="signup-section">
@@ -24,29 +33,34 @@
       <form action="loginb.php" method="post" id="signup-form">
         <input type="text" name="username" placeholder="Username" required>
         <input type="email" name="email" placeholder="Email" required>
-        <input type="password" name="password" placeholder="Password" required>
+        <input type="password" name="password" id="signup-password" placeholder="Password" required>
+        <input type="checkbox"  onclick="showPassword('signup-password')" style="display:inline;"> Show password
         <button type="submit">Sign Up</button>
         <p>Already have an account? <a href="#" id="login-link">Login</a></p>
       </form>
     </div>
   </div>
 
-  <script >
+  <script>
+    function showPassword(passwordId) {
+      var passwordField = document.getElementById(passwordId);
+      passwordField.type = passwordField.type === 'password' ? 'text' : 'password';
+    }
+
     const loginSection = document.getElementById('login-section');
-const signupSection = document.getElementById('signup-section');
-const signupLink = document.getElementById('signup-link');
-const loginLink = document.getElementById('login-link');
+    const signupSection = document.getElementById('signup-section');
+    const signupLink = document.getElementById('signup-link');
+    const loginLink = document.getElementById('login-link');
 
-signupLink.addEventListener('click', () => {
-  loginSection.classList.add('hidden');
-  signupSection.classList.remove('hidden');
-});
+    signupLink.addEventListener('click', () => {
+      loginSection.classList.add('hidden');
+      signupSection.classList.remove('hidden');
+    });
 
-loginLink.addEventListener('click', () => {
-  loginSection.classList.remove('hidden');
-  signupSection.classList.add('hidden');
-});
-
+    loginLink.addEventListener('click', () => {
+      loginSection.classList.remove('hidden');
+      signupSection.classList.add('hidden');
+    });
   </script>
 </body>
 </html>
