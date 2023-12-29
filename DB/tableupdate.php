f6987a0237d6ac9744c442150e68bfa73d3a84d3
shahid ali <shahid576ali@gmail.com>
2023-12-29 23:18:04 +0530

    .....

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "DELETE FROM users WHERE `users`.`id` = 37";
+    $sql = "DELETE FROM users WHERE `users`.`id` IN(11,16,18,19,20,21,22,23,24,25,29,30,31,32,33,34,35,36,44)";
 
     if($conn->query($sql)){
         echo "done ok";
