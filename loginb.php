7fd1f86b644528307e2bfa4fbe2f6a028610a654
shahid ali <shahid576ali@gmail.com>
2023-12-30 19:05:54 +0530

    ...

--- loginb.php
+++ loginb.php
@@ -13,54 +13,58 @@ if(isset($_REQUEST['username'])){
     $sql = "SELECT * FROM `users` WHERE email = '$email' ";
     $result = mysqli_query($conn ,$sql);
     $row  = mysqli_fetch_array($result);
+    
     if(is_array($row)) {
-        $_SESSION["message"] = "user all ready exist";
+        $_SESSION["message"] = "User already exists";
         echo '<script type="text/javascript">';
         echo 'window.location.href="login.php";';
         echo '</script>';
-    }else{
-        $email = $_REQUEST["email"];
-        $otp = rand(1111,9999);
-        $_SESSION["email"]=$email;
-        $_SESSION["otp"] = $otp; 
+    } else {
+        $otp = rand(1111, 9999);
+        $_SESSION["email"] = $email;
+        $_SESSION["otp"] = $otp;
         $to_email = $email;
-        $subject = "forget password";
-        $body = "Hi, you otp is $otp";
-        $headers = "From: contact@simtrak.in";
+        $subject = "Forget password";
+        $body = "Hi, your OTP is $otp";
+        $headers = "From: shahid576ali@gmail.com";
 
         if (mail($to_email, $subject, $body, $headers)) {
-                
-            header("Location:verify.php");
+            echo '<script type="text/javascript">';
+            echo 'window.location.href="verify.php";';
+            echo '</script>';
+        } else {
+            echo "Error found, check your internet connection";
+            exit();
+        }
+    }
+
+} else {
+    $sql = "SELECT * FROM users WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result);
+
+    if (is_array($row)) {
+        if ($row['password'] == $password) {
+            $_SESSION['user_id'] = $row["id"];
+            $_SESSION['role_id'] = $row["role_id"];
+            $_SESSION['user_name'] = $row["username"];
+            $sql = "SELECT * FROM role_teams WHERE user_id = '".$row["id"]."'";
+            $result = mysqli_query($conn, $sql);
+            $row = mysqli_fetch_array($result);
+            $_SESSION['team_id'] = $row["team_id"];
+            header("Location:index.php");
             exit();
         } else {
-            echo "error found check your internet connection";
-           // header("Location:sendotp.php");
+            $_SESSION["message"] = "Invalid Password!";
+            $_SESSION['username'] = $_POST['username'];
+            header("Location:login.php");
             exit();
         }
-    
-    }
-}else{
-    $sql = "SELECT * FROM `users` WHERE email = '$email' and password='$password'";
-    $result = mysqli_query($conn ,$sql);
-    $row  = mysqli_fetch_array($result);
-    if(is_array($row)) {
-        $_SESSION['user_id'] = $row["id"];
-        $_SESSION['role_id'] = $row["role_id"];
-        $_SESSION['user_name'] = $row["username"];
-        $sql = "SELECT * FROM `role_teams` WHERE `user_id` = '".$row["id"]."'";
-        $result = mysqli_query($conn ,$sql);
-        $row  = mysqli_fetch_array($result);
-        $_SESSION['team_id'] = $row["team_id"];
-        header("Location:index.php");
-        exit();
     } else {
-        $_SESSION["message"] = "Invalid Username or Password!";
+        $_SESSION["message"] = "Invalid Username!";
+        $_SESSION['username'] = $_POST['username'];
         header("Location:login.php");
         exit();
-        }
+    }
 }
-
-
-
-
 ?>
