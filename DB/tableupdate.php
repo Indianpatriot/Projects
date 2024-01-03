4432832145d0db703802404e8e7b79f5fd0a1606
shahid ali <shahid576ali@gmail.com>
2024-01-03 20:11:20 +0530

    ..

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "DELETE FROM `UPDATE `teams` SET `Status` = 'Active' WHERE `teams`.`id` = 8`";
+    $sql = "UPDATE `teams` SET `Status` = 'Active' WHERE `teams`.`id` = 8`";
 
     if($conn->query($sql)){
         echo "done ok";
