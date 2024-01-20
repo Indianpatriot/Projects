c99977823c554fac92418850992ec3c5dd7138ab
shahid ali <shahid576ali@gmail.com>
2024-01-20 17:01:14 +0530

    ...

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <html>
 <head>
   <title>Goal Management Dashboard</title>
-  <link rel="icon" href="image/icon.png"  type="image/png" />>
+  <link rel="icon" href="image/icon.png"  type="image/png" />
   <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="styles.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
