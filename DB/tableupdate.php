35efe7d47a4d680c7e7e8c81ec43827d6f262d03
shahid ali <shahid576ali@gmail.com>
2024-01-24 21:20:18 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "INSERT INTO `Glowball`(`goalset`) VALUES ('1')";
+    $sql = "INSERT INTO `Lset Institution Development`(`goalset`) VALUES ('1')";
 
     if($conn->query($sql)){
         echo "done ok";
