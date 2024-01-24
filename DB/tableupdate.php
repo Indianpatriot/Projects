e2fd5e176459ec6756644a3db71eee74a443e0fa
shahid ali <shahid576ali@gmail.com>
2024-01-24 21:25:58 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "ALTER TABLE `Ascend` ADD `Number of New Volunteers` INT NOT NULL AFTER `Member Name`;";
+    $sql = "ALTER TABLE `Lset Institution Development` ADD `Number of New Volunteers` INT NOT NULL AFTER `Member Name`;";
 
     if($conn->query($sql)){
         echo "done ok";
