5073a8f16d1b992ea7ac70f24c3c371f40e0dfe2
shahid ali <shahid576ali@gmail.com>
2024-01-24 21:02:48 +0530

    ..

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "INSERT INTO `LSET`(`goalset`) VALUES ('1')";
+    $sql = "ALTER TABLE `LSET` ADD `Number of New Volunteers` INT(10) NOT NULL AFTER `Member Name`;";
 
     if($conn->query($sql)){
         echo "done ok";
