9a94b8031cf768fd6f92a81eeff0560d68aa87a0
shahid ali <shahid576ali@gmail.com>
2024-01-25 20:57:23 +0530

    ....

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "DELETE FROM `LSET` WHERE `LSET`.`parameter_id` = '88'";
+    $sql = "DELETE FROM `goal_parameter` WHERE `parameter_id` = '88'";
 
     if($conn->query($sql)){
         echo "done ok";
