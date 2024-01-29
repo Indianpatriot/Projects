caa3265ef728dcaf3723881855b002dbaaf2942f
shahid ali <shahid576ali@gmail.com>
2024-01-29 18:10:17 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "ALTER TABLE `Ascend` CHANGE `ID` `ID` INT(10) NOT NULL AUTO_INCREMENT, add PRIMARY KEY (`ID`);";
+    $sql = "ALTER TABLE `Glowball` CHANGE `ID` `ID` INT(10) NOT NULL AUTO_INCREMENT, add PRIMARY KEY (`ID`);";
 
     if($conn->query($sql)){
         echo "done ok";
