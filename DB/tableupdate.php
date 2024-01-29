195f45a7167fff303922d687ba8e512f67194227
shahid ali <shahid576ali@gmail.com>
2024-01-29 19:15:45 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "delete from `teams` where `id` = '7';";
+    $sql = "delete from `teams` where `id` = '9';";
 
     if($conn->query($sql)){
         echo "done ok";
