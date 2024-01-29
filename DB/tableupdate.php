a010415538e647f063fad4401766e7728db09949
shahid ali <shahid576ali@gmail.com>
2024-01-29 18:03:14 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "INSERT INTO `LSET` (`ID`, `Member ID`, `Member Name`, `Date`, `New tasks received`, `Tasks in progress asaadasadaasdasdasda`, `Continuous tasks`, `Tasks completed today`, `Article recieved`, `Cities initiated`, `Cities started`, `goalset`) VALUES (NULL, '', '', current_timestamp(), '', '', '', '', '', NULL, '', '1')";
+    $sql = "INSERT INTO `LSET` (`goalset`) VALUES ('1')";
 
     if($conn->query($sql)){
         echo "done ok";
