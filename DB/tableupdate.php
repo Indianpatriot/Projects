782f0955ad07a5a6015d278e504d4af680f9a497
shahid ali <shahid576ali@gmail.com>
2024-02-06 21:20:00 +0530

    ...

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "ALTER TABLE `LSET` DROP `Number of New Volunteer`";
+    $sql = "UPDATE `users` SET `username`='Priti sinha' WHERE `id`= '54'";
 
     if($conn->query($sql)){
         echo "done ok";
