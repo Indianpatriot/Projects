dc84ec028528a5a5267cdef9033d1533819189da
shahid ali <shahid576ali@gmail.com>
2024-02-09 23:16:44 +0530

    ...

--- periousmonthb.php
+++ periousmonthb.php
@@ -6,6 +6,7 @@ $year = date('Y');
 $month = date('n');
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $month = $_POST["select_month"];
+    define("month", $_POST["select_month"]);
     $year = $_POST["select_year"];
     echo "ok";
 }
