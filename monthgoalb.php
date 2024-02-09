ee228377ca86ce51943a590dfa7ca3b5435b5b0e
shahid ali <shahid576ali@gmail.com>
2024-02-09 21:21:21 +0530

    ....

--- monthgoalb.php
+++ monthgoalb.php
@@ -2,13 +2,21 @@
 include("DB/dbconn.php");
 
 $username = $_SESSION['user_id'];
-$total = array([],[]);
 // print team goal
 $teamID = $_SESSION["team_id"];
 $sql1 = "SELECT * FROM `teams` WHERE `id` = '$teamID'";
 $teamname = mysqli_query($conn,$sql1);
 $teamname = mysqli_fetch_object($teamname);
-
+$goal_parameter = "SELECT * FROM `goal_parameter` WHERE team_id ='0' OR team_id =".$_SESSION["team_id"]." ORDER BY `goal_parameter`.`team_id` ASC ";
+    $parameter = mysqli_query($conn,$goal_parameter);
+    $parameters = mysqli_query($conn,$goal_parameter);
+    $goalparameters = mysqli_query($conn,$goal_parameter);
+    $array = array();
+    $i = 0;
+    while($para = mysqli_fetch_object($parameter)){
+      $array[$i] = $para->parameter;
+      $i++;
+    }
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $updategoal = "UPDATE `$teamname->team_name` SET `Member ID` = ".$_REQUEST["team_manager_id"].", `Member Name`= '".$_REQUEST["team_manager_name"]."' WHERE `goalset` = '1'";
     if(mysqli_query($conn,$updategoal)){
