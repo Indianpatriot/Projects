62e20c7ad2a992a7599fa645e285a6ef54484d71
shahid ali <shahid576ali@gmail.com>
2024-02-09 21:49:26 +0530

    ....

--- monthgoalb.php
+++ monthgoalb.php
@@ -1,40 +1,53 @@
 <?php
 include("DB/dbconn.php");
 
+session_start(); // Start the session to access session variables
 $username = $_SESSION['user_id'];
-// print team goal
+
+// Fetch team goal
 $teamID = $_SESSION["team_id"];
 $sql1 = "SELECT * FROM `teams` WHERE `id` = '$teamID'";
-$teamname = mysqli_query($conn,$sql1);
-$teamname = mysqli_fetch_object($teamname);
-$goal_parameter = "SELECT * FROM `goal_parameter` WHERE team_id ='0' OR team_id =".$_SESSION["team_id"]." ORDER BY `goal_parameter`.`team_id` ASC ";
-    $parameter = mysqli_query($conn,$goal_parameter);
-    $parameters = mysqli_query($conn,$goal_parameter);
-    $goalparameters = mysqli_query($conn,$goal_parameter);
-    $array = array();
-    $i = 0;
-    while($para = mysqli_fetch_object($parameter)){
-      $array[$i] = $para->parameter;
-      $i++;
-    }
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $updategoal = "UPDATE `$teamname->team_name` SET `Member ID` = ".$_REQUEST["team_manager_id"].", `Member Name`= '".$_REQUEST["team_manager_name"]."' WHERE `goalset` = '1'";
-    if(mysqli_query($conn,$updategoal)){
-        $i=0;  
-        foreach($array as $value){
-            if($value == 'Date' || $value == 'Member Name'){$i++; continue;}
-            $updategoal = "UPDATE `$teamname->team_name` SET `$value`= '".$_REQUEST[$i]."' WHERE `goalset` = '1'";
-            if(mysqli_query($conn,$updategoal)){
-            }else{
+$teamname_result = mysqli_query($conn, $sql1);
+$teamname_obj = mysqli_fetch_object($teamname_result);
+$teamname = $teamname_obj->team_name;
+
+$goal_parameter = "SELECT * FROM `goal_parameter` WHERE team_id ='0' OR team_id = '$teamID' ORDER BY `goal_parameter`.`team_id` ASC ";
+$parameter_result = mysqli_query($conn, $goal_parameter);
+
+$parameters = array();
+$goalparameters = array();
+$array = array();
+$i = 0;
+
+while ($para = mysqli_fetch_object($parameter_result)) {
+    $parameters[] = $para;
+    $array[$i] = $para->parameter;
+    $i++;
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $team_manager_id = $_POST["team_manager_id"];
+    $team_manager_name = $_POST["team_manager_name"];
+
+    $updategoal = "UPDATE `$teamname` SET `Member ID` = '$team_manager_id', `Member Name`= '$team_manager_name' WHERE `goalset` = '1'";
+
+    if (mysqli_query($conn, $updategoal)) {
+        $i = 0;
+        foreach ($array as $value) {
+            if ($value == 'Date' || $value == 'Member Name') {
+                $i++;
+                continue;
+            }
+            $parameter_value = $_POST[$i];
+            $updategoal = "UPDATE `$teamname` SET `$value`= '$parameter_value' WHERE `goalset` = '1'";
+            if (!mysqli_query($conn, $updategoal)) {
                 echo $conn->error;
             }
             $i++;
-        }  
-        echo "ok"
-    }else{
-        echo "error";
+        }
+        echo "ok"; // Send response to JavaScript
+    } else {
+        echo "error"; // Send response to JavaScript
     }
 }
-
-
 ?>
