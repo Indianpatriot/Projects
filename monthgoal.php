076ac8ea9b3e05ef5e9dd3b1fa404fa6a2f77d39
shahid ali <shahid576ali@gmail.com>
2024-02-09 21:17:53 +0530

    ...

--- monthgoal.php
+++ monthgoal.php
@@ -395,6 +395,9 @@ function myFunctiosn1() {
 <div class="all-form-element-inner">
   <div id="formbox">
     <form id="uploadForm" enctype="multipart/form-data">
+    <input type="text" name="team_manager_id" value="<?=$_SESSION['user_id']?>" hidden require>
+                  <input type="text" name="team_manager_name" value="<?=$_SESSION['user_name']?>" hidden require>
+                  
     <?php $c=0; while($para = mysqli_fetch_object($parameters)){ ?>
     <?php if($para->parameter == 'Date' || $para->parameter == 'Member Name'){$i++; continue;}  ?>
       <div class="form-group-inner">
