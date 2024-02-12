8aa69f900d1821c4d7261d9dd203c72a669f7640
shahid ali <shahid576ali@gmail.com>
2024-02-12 21:01:03 +0530

    ...

--- reorderparameterb.php
+++ reorderparameterb.php
@@ -1,7 +1,7 @@
 <?php
 include("DB/dbconn.php");
 $team_id = $_SESSION["team_id"];
-$goal_parameter = "SELECT * FROM `goal_parameter` WHERE team_id ='0' OR team_id ="$team_id" ORDER BY `goal_parameter`.`team_id` ASC ";
+$goal_parameter = "SELECT * FROM `goal_parameter` WHERE team_id ='0' OR team_id ='$team_id' ORDER BY `goal_parameter`.`team_id` ASC ";
 $parameter = mysqli_query($conn,$goal_parameter);
 while($para = mysqli_fetch_object($parameter)){
     $array[$i] = $para->parameter;
