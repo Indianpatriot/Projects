19c753c5f8c8910580390014c7a2f4c4f1a77953
shahid ali <shahid576ali@gmail.com>
2024-02-12 18:35:39 +0530

    ...

--- editteamb.php
+++ editteamb.php
@@ -4,10 +4,14 @@ if(isset($_POST["team_domain"])){
     $teamedit = "UPDATE `teams` SET `team_name` = '".$_POST["team_name"]."', `team_domain` = '".$_POST["team_domain"]."' WHERE `teams`.`id` = '".$_POST["team_id"]."'";
     $changeteamname = "RENAME TABLE `".$_POST["team_old"]."` TO `".$_POST["team_name"]."`";
     if(mysqli_query($conn ,$teamedit)){
-        if(mysqli_query($conn ,$changeteamname)){
+        if($_POST["team_old"] == $_POST["team_name"]){
             echo "ok";
         }else{
-            echo $conn->error;
+            if(mysqli_query($conn ,$changeteamname)){
+                echo "ok";
+            }else{
+                echo $conn->error;
+            }
         }
     }else{
         echo $conn->error;
