5dc721c19156c9ee3006a2e08941bc31d6a2cb1b
shahid ali <shahid576ali@gmail.com>
2024-02-13 17:06:58 +0530

    ...

--- index.php
+++ index.php
@@ -295,8 +295,8 @@ $(document).ready(function() {
               <td><?php echo $table->team_name; ?></td>
               <td><?php echo $table->team_domain; ?></td>
               <?php if($_SESSION['role_id']==1){ ?>
-                <td><a href="#" onclick="window.open('teamstatus.php?team_id=<?=$table->id?>', 'newwindow', 'width=500,height=500'); return false;"><?php echo $table->Status; ?></a></td>
-                <td><a href="#" onclick="window.open('editteam.php?team_id=<?=$table->id?>', 'newwindow', 'width=500,height=500'); return false;">Edit Team</a></td>
+                <td><a href="#" class="btn btn-primary" title="Status Change" onclick="window.open('teamstatus.php?team_id=<?=$table->id?>', 'newwindow', 'width=500,height=500'); return false;"><i class='fa fa-gg-circle' aria-hidden='true'></i></a></td>
+                <td><a href="#" class="btn btn-primary" title="Status Change" onclick="window.open('editteam.php?team_id=<?=$table->id?>', 'newwindow', 'width=500,height=500'); return false;">Edit Team</a></td>
               <?php } ?>
             </tr>
             <?php } ?>
