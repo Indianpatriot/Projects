5aec04717ed910405a5b2dedaeb02898fcbed39f
shahid ali <shahid576ali@gmail.com>
2024-02-13 18:40:28 +0530

    ...

--- addteam.php
+++ addteam.php
@@ -5,7 +5,7 @@
   ?>
 
 <!doctype html>
-<title>Team Create/Add Parameter |  GOAL MANAGEMENT </title>
+<title>Create Team/Add Parameter |  GOAL MANAGEMENT </title>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- select2 CSS
@@ -377,7 +377,7 @@ function myFunctiosn1() {
 <div style="margin-top:0px;margin-bottom:20px;" class="income-dashone-total shadow-reset nt-mg-b-30">
 <div class="income-title">
 <div class="main-income-head">
-<h2>Team Create/Add Parameter</h2>
+<h2>Create Team/Add Parameter</h2>
 </div>
 </div>
 <div class="sparkline10-graph">
