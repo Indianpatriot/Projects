3ee6bdb50cded60e5ef50f9e70eaf90062417a17
shahid ali <shahid576ali@gmail.com>
2024-02-16 17:21:03 +0530

    ..

--- UI.php
+++ UI.php
@@ -311,7 +311,7 @@ $(document).ready(function() {
             </tr>
             </thead>
             <tbody>
-            <tr>
+            <tr style="position:fixed">
               <th>Total History</th>
               <td></td>
               <?php
