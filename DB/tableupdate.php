02a5f8a0f40579ecb08382f987ed092bea9f54eb
shahid ali <shahid576ali@gmail.com>
2024-02-16 19:44:26 +0530

    ....

--- DB/tableupdate.php
+++ DB/tableupdate.php
@@ -3,7 +3,7 @@
 
     include("dbconn.php");
 
-    $sql = "ALTER TABLE `Ascend` ADD `Remark` VARCHAR(100) NOT NULL AFTER `goalset`";
+    $sql = "ALTER TABLE `Matrix` ADD `Remark` VARCHAR(100) NOT NULL AFTER `goalset`";
 
     if($conn->query($sql)){
         echo "done ok";
