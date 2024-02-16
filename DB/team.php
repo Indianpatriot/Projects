668a524d86eec73cab361da1a358d11b2a09b8e3
shahid ali <shahid576ali@gmail.com>
2024-02-16 16:59:07 +0530

    ....

--- DB/team.php
+++ DB/team.php
@@ -2,9 +2,9 @@
 
     include("dbconn.php");
     if($_SESSION['role_id']==1){
-        $sql = "SELECT LPAD(id, 4, '0') AS id , `team_name`, `team_domain`, `Status` FROM `teams`";
+        $sql = "SELECT LPAD(id, 4, '0') AS id , `team_name`, `team_domain`, `Status`, `Target`,`Target_achiv` FROM `teams`";
     }else{
-        $sql = "SELECT LPAD(id, 4, '0') AS id , `team_name`, `team_domain`, `Status` FROM `teams` where `Status` ='Active'";
+        $sql = "SELECT LPAD(id, 4, '0') AS id , `team_name`, `team_domain`, `Status`,`Target`,`Target_achiv` FROM `teams` where `Status` ='Active'";
     }
     $results = mysqli_query($conn ,$sql);
     $teamactive = mysqli_query($conn ,$sql);
