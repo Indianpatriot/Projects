16fb6aafade2fb66a478c80562e860cbdd8df0ad
shahid ali <shahid576ali@gmail.com>
2024-02-16 19:22:43 +0530

    ....

--- DB/rowcount.php
+++ DB/rowcount.php
@@ -1,7 +1,8 @@
 <?php
 include("dbconn.php");
 
-$managercheck = "SELECT * FROM `role_teams` where `tema_id` = '1' AND `role_id` = '3'";
+$managercheck = "SELECT * FROM `role_teams` where `team_id` = '1' AND `role_id` = '3'";
+$managercheck = mysqli_query($conn,$managercheck);
 $managercheck = mysqli_num_rows($managercheck);
 echo $managercheck;
 echo $conn->error;
--- addmemberb.php
+++ addmemberb.php
@@ -16,7 +16,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $sql35 = "SELECT * FROM `users` WHERE `id` = '$id'";
     $result35 = mysqli_query($conn, $sql35);
     $row = mysqli_fetch_object($result35);
-            $managercheck = "SELECT * FROM `role_teams` where `tema_id` = '$teamID' AND `role_id` = '$role_id'";
+            $managercheck = "SELECT * FROM `role_teams` where `team_id` = '$teamID' AND `role_id` = '$role_id'";
             $managercheck = mysqli_query($conn,$managercheck);
             $managercheck = mysqli_num_rows($managercheck);
             if($managercheck >=1){
