6fe269067d23993651bf0e6d7f9bd81ef7cf5fb2
shahid ali <shahid576ali@gmail.com>
2024-02-16 17:18:17 +0530

    ...

--- UI.php
+++ UI.php
@@ -312,7 +312,7 @@ $(document).ready(function() {
             </thead>
             <tbody>
             <tr>
-              <th>Total History</th>
+              <th style="position:fixed">Total History</th>
               <td></td>
               <?php
               foreach($array as $value){ if($value=="Date" || $value == "Member Name"){continue;}?>
