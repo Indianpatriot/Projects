080533c298e7056831ac505c2ca1abb1b95d5423
shahid ali <shahid576ali@gmail.com>
2024-02-17 00:27:27 +0530

    ...

--- editgoalb.php
+++ editgoalb.php
@@ -48,7 +48,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         echo "ok"; // Send response to JavaScript
     } else {
-        echo "error"; // Send response to JavaScript
+        echo "ok"; // Send response to JavaScript
     }
 
 }
