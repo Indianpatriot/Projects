a2d33581da63204407e6bcd70b708bbe0a1b42bc
shahid ali <shahid576ali@gmail.com>
2024-02-16 19:17:46 +0530

    ...

---
+++ DB/rowcount.php
@@ -0,0 +1,9 @@
+<?php
+include("dbconn.php");
+
+$managercheck = "SELECT * FROM `role_teams` where `tema_id` = '1' AND `role_id` = '3'";
+$managercheck = mysqli_num_rows($managercheck);
+echo $managercheck;
+
+
+?>
