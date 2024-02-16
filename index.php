7d5d0cf339bf4adc50ab917de9e9a2c557d397f5
shahid ali <shahid576ali@gmail.com>
2024-02-16 16:56:30 +0530

    ...

--- index.php
+++ index.php
@@ -280,7 +280,7 @@ $(document).ready(function() {
               <?php $ta = $table->Target_achiv;
                     $t = $table->Target;
               $achivement = (251/1000)*100; if($achivement <= 25 ){ ?>
-              <td><span><div class="pie animate no-round" style="--p:<?=$achivement?>;--c:red;"> <?=$achivement?>% </div></span></td>
+              <td><span><div class="pie animate no-round" style="--p:<?=$achivement?>;--c:red;"> <?=$t?>% </div></span></td>
               <?php }elseif($achivement <= 50 ){ ?>
               <td><span><div class="pie animate no-round" style="--p:<?=$achivement?>;--c:orange;"> <?=$achivement?>% </div></span></td>
               <?php }elseif($achivement <= 75 ){ ?>
