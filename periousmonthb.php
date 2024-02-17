7a6ae0e37fc4d460697b392c8787bf977680f5f5
shahid ali <shahid576ali@gmail.com>
2024-02-17 18:10:59 +0530

    ..

--- periousmonthb.php
+++ periousmonthb.php
@@ -12,7 +12,11 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $year = $_SESSION["year"];
     $start_date = $year . '-' . str_pad($month, 2, '0', STR_PAD_LEFT) . '-01';
     $end_date = date('Y-m-t', strtotime($start_date));
+    if($_SESSION['role_id'] != 4){
     $sql2 = "SELECT * FROM `$teamname` WHERE `goalset` <> '1' AND `Date` BETWEEN '$start_date' AND '$end_date' ORDER BY `Date` DESC";
+    }else{
+        $sql2 = "SELECT * FROM `$teamname` WHERE `goalset` <> '1' AND `Date` BETWEEN '$start_date' AND `` AND '$end_date' ORDER BY `Date` DESC";
+    }
     $results = mysqli_query($conn ,$sql2);
 
 ?>
