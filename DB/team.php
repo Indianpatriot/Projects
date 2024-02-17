a298d5557233a4976772cbb715f83aa8ccb7e7cb
shahid ali <shahid576ali@gmail.com>
2024-02-17 17:41:21 +0530

    ...

--- DB/team.php
+++ DB/team.php
@@ -8,12 +8,12 @@
         $selectedteam = mysqli_query($conn,$selectedteam);
         $selectids = array();
         $i = 0;
-        // while($selecteteam = mysqli_fetch_object($selectedteam)){
-        //    $selectids[$i] = $selecteteam->team_id;
-        //    $i++; 
-        // }
-        // $selectid = "(" . implode(",", $selectids) . ")";
-        $sql = "SELECT LPAD(id, 4, '0') AS id , `team_name`, `team_domain`, `Status`,`team_manager`,`team_coordinator`,`Target`,`Target_achiv` FROM `teams` where `Status` ='Active' AND `id` = '1'";
+        while($selecteteam = mysqli_fetch_object($selectedteam)){
+           $selectids[$i] = $selecteteam->team_id;
+           $i++; 
+        }
+        $selectid = "(" . implode(",", $selectids) . ")";
+        $sql = "SELECT LPAD(id, 4, '0') AS id , `team_name`, `team_domain`, `Status`,`team_manager`,`team_coordinator`,`Target`,`Target_achiv` FROM `teams` where `Status` ='Active' AND `id` IN $selectid";
     }
     $results = mysqli_query($conn ,$sql);
     $teamactive = mysqli_query($conn ,$sql);
