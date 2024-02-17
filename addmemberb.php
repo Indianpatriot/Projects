92ba49dc94473c8459df3caf5d4bde3563117b71
shahid ali <shahid576ali@gmail.com>
2024-02-17 16:27:13 +0530

    ...

--- addmemberb.php
+++ addmemberb.php
@@ -25,7 +25,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             }else{
                 $sql2 = "INSERT INTO `role_teams` (`role_id`, `team_id`, `user_id`) VALUES ('$role_id', '$teamID', '$id')";
                 $sql3 = "UPDATE `users` SET `role_id`='3' WHERE `id` = '$userId'";
-                if(mysqli_query($conn, $sql2) && mysqli_query($conn, $sql3)){
+                if(mysqli_query($conn, $sql3)){
                     echo "ok";
                 } 
             }
