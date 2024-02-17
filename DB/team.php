f05e9d0c5b4ea5a9e33293ed0f11901a7b251a46
shahid ali <shahid576ali@gmail.com>
2024-02-17 17:40:41 +0530

    ...

--- DB/team.php
+++ DB/team.php
@@ -5,9 +5,9 @@
         $sql = "SELECT LPAD(id, 4, '0') AS id , `team_name`, `team_domain`, `Status`,`team_manager`,`team_coordinator`, `Target`,`Target_achiv` FROM `teams`";
     }else{
         $selectedteam = "SELECT * FROM `role_teams` WHERE `user_id` = '".$_SESSION['user_id']."'";
-        // $selectedteam = mysqli_query($conn,$selectedteam);
-        // $selectids = array[];
-        // $i = 0;
+        $selectedteam = mysqli_query($conn,$selectedteam);
+        $selectids = array();
+        $i = 0;
         // while($selecteteam = mysqli_fetch_object($selectedteam)){
         //    $selectids[$i] = $selecteteam->team_id;
         //    $i++; 
