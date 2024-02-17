ae741bffda7e8390b7b62aae93c71931bb37441b
shahid ali <shahid576ali@gmail.com>
2024-02-17 16:21:02 +0530

    ...

--- addmemberb.php
+++ addmemberb.php
@@ -32,7 +32,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }elseif($row->role_id=="3"){
         $sql2 = "INSERT INTO `role_teams` (`role_id`, `team_id`, `user_id`) VALUES ('$role_id', '$teamID', '$id')";
                 if(mysqli_query($conn, $sql2)){
-                    echo "ok";
+                    echo "oka";
                 } 
     }else{
         $sql2 = "INSERT INTO `role_teams` (`role_id`, `team_id`, `user_id`) VALUES ('$role_id', '$teamID', '$id')";
