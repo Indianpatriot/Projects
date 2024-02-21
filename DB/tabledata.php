31bae67eb2dac2940daf4c054afe228499250098
shahid ali <shahid576ali@gmail.com>
2024-02-21 22:09:01 +0530

    ...

--- DB/tabledata.php
+++ DB/tabledata.php
@@ -1,8 +1,8 @@
 <?php
 
-include("dbconna.php");
+include("dbconn.php");
 
-$query = "SELECT * FROM `LSET`";
+$query = "SELECT * FROM `teams`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
