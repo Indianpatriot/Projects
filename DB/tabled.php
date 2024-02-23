1f914237ab84610cc7c88cc0b825cc5fbe554db2
shahid ali <shahid576ali@gmail.com>
2024-02-23 17:12:01 +0530

    ...

--- DB/tabled.php
+++ DB/tabled.php
@@ -2,7 +2,7 @@
 
 include("dbconn.php");
 
-$query = "SELECT * FROM `pinnacle 1`";
+$query = "SELECT * FROM `teams`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
