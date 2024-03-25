9deffd177d29ee30c4145f3ce5a61ea9f882eed2
shahid ali <shahid576ali@gmail.com>
2024-03-25 20:08:30 +0530

    ...

--- DB/tabled.php
+++ DB/tabled.php
@@ -2,7 +2,7 @@
 
 include("dbconn.php");
 
-$query = "SELECT * FROM `teams`";
+$query = "SELECT * FROM `users`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
