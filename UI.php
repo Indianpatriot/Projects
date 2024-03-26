5e7260f4c208b9ddedfcacb0966b91475e1305f3
shahid ali <shahid576ali@gmail.com>
2024-03-26 20:20:55 +0530

    ...

--- UI.php
+++ UI.php
@@ -375,8 +375,8 @@ include ("periousmonthb.php");
                       onclick="window.open('updategoal.php', 'newwindow', 'width=500,height=500'); return false;"
                       class="btn btn-primary">Add Goal</a>
                     <table width="100%" id="ssss" class="table table-striped table-bordered">
-                      <thead style="position: sticky; top: 0;">
-                        <tr>
+                      <thead>
+                        <tr  style="position: sticky; top: 0;">
                           <?php
                           foreach ($array as $value) { ?>
                             <th>
