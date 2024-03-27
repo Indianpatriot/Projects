a1d7772c61538574a48379512d639d36a38deb01
shahid ali <shahid576ali@gmail.com>
2024-03-27 22:18:29 +0530

    ...

--- Untitled-1b.php
+++ Untitled-1b.php
@@ -105,8 +105,9 @@ while (isset($_REQUEST[$z])) {
 // update goal date wise
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $date_data = $_REQUEST["date_data"];
-    $result = "`Date`,`" . implode("`,`", $goalp) . "`)";
-    $results = "'$date_data','" . implode("','", $inputpara) . "')";
+    $Remark = $_REQUEST["Remark"];
+    $result = "`Date`,`" . implode("`,`", $goalp) . "`,`Remark`)";
+    $results = "'$date_data','" . implode("','", $inputpara) . "','$Remark')";
 
     if (isset($_REQUEST["membername"])) {
         $i = $_REQUEST["membername"];
--- updategoal.php
+++ updategoal.php
@@ -482,6 +482,17 @@ if (isset($_GET["select_month"])) {
                         </div>
                       </div>
                     <?php } ?>
+                    <div class="form-group-inner">
+                        <div class="row">
+                          <div class="col-lg-3">
+                            <label class="login2 pull-right pull-right-pro"><?= $para->parameter ?>:</label>
+                          </div>
+                          <div class="col-lg-9">
+                            <input type="text" placeholder="Remark"
+                              class="form-control" id="task_name" name="Remark" required>
+                          </div>
+                        </div>
+                      </div>
                     <center>
                       <button type="submit" class="btn btn-primary"><span class="fa fa-check"></span>&nbsp
                         Submit</button>
