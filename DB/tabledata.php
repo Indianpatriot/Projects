7c39f88f4c3e03b9d98ebc81161941a1f65c3d25
shahid ali <127850598+shahid576ali@users.noreply.github.com>
2024-06-03 20:55:30 +0530

    Update tabledata.php

--- DB/tabledata.php
+++ DB/tabledata.php
@@ -2,7 +2,7 @@
 
 include("dbconn.php");
 
-$query = "SELECT * FROM `Vision`";
+$query = "SELECT * FROM `LSET`";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
     // Fetch the result as an associative array
