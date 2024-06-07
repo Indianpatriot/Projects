9e9cabeb0b3e0b10608fc6cea3aa8b240c8090b3
shahid576ali <127850598+shahid576ali@users.noreply.github.com>
2024-06-07 19:38:19 +0530

    ...

--- Untitled-1b.php
+++ Untitled-1b.php
@@ -287,38 +287,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 $normalmember = "SELECT * FROM `users` WHERE `role_id` IN (3, 5) ORDER BY id ASC";
 $normaladdmember = mysqli_query($conn, $normalmember);
 
-if (isset($_GET["membername"])) {
-    $id = $_GET["membername"];
-    $role_id = $_GET["membertype"];
-    $sql35 = "SELECT * FROM `users` WHERE `id` = '$id'";
-    $result35 = mysqli_query($conn, $sql35);
-    $row = mysqli_fetch_object($result35);
-    if ($row->role_id == 3) {
-        if ($role_id == 3) {
-            $sql2 = "INSERT INTO `role_teams` (`role_id`, `team_id`, `user_id`) VALUES ('$role_id', '$teamID', '$id')";
-            if (mysqli_query($conn, $sql2)) {
-                echo '<script type="text/javascript">';
-                echo 'window.location.href="Untitled-1.php";';
-                echo '</script>';
-            }
-        } else {
-            $_SESSION["team_manager"] = "$row->username is a team manager , can't be a member.";
-            echo '<script type="text/javascript">';
-            echo 'window.location.href="Untitled-1.php";';
-            echo '</script>';
-        }
-    } else {
-        $sql1 = "UPDATE `users` SET `role_id`='$role_id' WHERE `id` = '$id' ";
-        $sql2 = "INSERT INTO `role_teams` (`role_id`, `team_id`, `user_id`)
-        VALUES ('$role_id', '$teamID', '$id')";
-        if (mysqli_query($conn, $sql1) && mysqli_query($conn, $sql2)) {
-            echo '<script type="text/javascript">';
-            echo 'window.location.href="Untitled-1.php";';
-            echo '</script>';
-        }
-    }
-}
-
 // Create a string with array elements enclosed in parentheses and separated by commas
 $printid = "SELECT * FROM `role_teams` WHERE `role_id` IN (3,4) AND `team_id` ='$teamID'  ORDER BY `role_id`";
 $printidselect = mysqli_query($conn, $printid);
