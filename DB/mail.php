c43c77c62275c88dd9ac71f90682249ec99c797a
shahid576ali <127850598+shahid576ali@users.noreply.github.com>
2024-06-11 21:03:11 +0530

    ...

---
+++ DB/mail.php
@@ -0,0 +1,11 @@
+
+
+<?php
+$to = "shadowchor883@gmail.com";
+$subject = "check kr rha hu";
+$body = "bus dekh rha tha ki cc m kaise kr kr skta hu";
+$from = "From: shahid576ali@gmail.com \r\n";
+$from = "cc: simran576ali@gmail.com";
+mail($to,$subject,$body,$from);
+
+?>
